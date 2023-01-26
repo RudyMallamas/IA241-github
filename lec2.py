78914a5cde8714654c41c7dc0b5a8f23573b28e6
EC2 Default User <ec2-user@ip-172-31-81-237.ec2.internal>
2023-01-26 21:08:52 +0000

    lec2

---
+++ lec2.py
@@ -0,0 +1,28 @@
+'''
+lec 2
+numbers, strings, and variables
+regional
+'''
+
+#print(  type('Hello')   )  
+
+#print(  type(123)    )
+
+#print(  type(123.123)   )
+
+#print('hello')
+#print("hello")
+#print("it's our 2nd pyhton class")
+
+#print('Hello World')
+#print('Hello World'.lower())
+#print('   Hello World'.strip())
+
+#print('hello world'.split()) # split by spaces 
+#print('hello world'.split('h')) # split on characters 
+#print('hello' + ' world')
+
+#print(1 + 2 - 3 * 69 / 4 + 2.2) #math
+
+my_num = 456
+print(my_num)
